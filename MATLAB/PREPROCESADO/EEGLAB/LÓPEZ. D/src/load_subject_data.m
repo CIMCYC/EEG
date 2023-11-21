360e47c96f39e895fa86681d8a1abb56dda2224c
ruizromeerom <125973650+ruizromeerom@users.noreply.github.com>
2023-11-21 11:56:05 +0100

    Update load_subject_data.m

--- MATLAB/PREPROCESADO/EEGLAB/LÓPEZ. D/src/load_subject_data.m
+++ MATLAB/PREPROCESADO/EEGLAB/LÓPEZ. D/src/load_subject_data.m
@@ -10,32 +10,42 @@ function data = load_subject_data(cfg,subject)
 %  subject.
 
 fprintf(['\n<strong> > Loading subject: ' subject.id '</strong>\n\n']);
+%
 
 % Search the .vhdr file:
-file = dir([subject.dir filesep 'sub*.vhdr']);
+file = dir([subject.raw_dir filesep 'sub*.vhdr']);
+cont=0;
 
 % If vhdr file is found, load subject data:
 if size(file,1)
     
     % Load subject data:
     first_sample_to_read = 1;
-    data = pop_loadbv(subject.dir, file(1).name, first_sample_to_read,...
+    data = pop_loadbv(subject.raw_dir, file(1).name, first_sample_to_read,...
         cfg.chantoload);
-    
-else
-    
+%
+elseif cont==0
+    % Search the .dap file:
+    file=dir([subject.raw_dir filesep 'sub*.dap']);
+
+    if  [ize(file,1) & (file.name=='sub*.dap')]
+        % Load subject data:
+        first_sample_to_read = 1;
+        data = pop_loadcurry([subject.raw_dir filesep file(1).name]);
+
+    else
     % Search .set file:
-    file = dir([subject.dir filesep 'sub*.set']);
+        file = dir([subject.raw_dir filesep 'sub*.set']);
     
-    if size(file,1)
+        if size(file,1)
         % Load subject data:
         data = pop_loadset('filename',file(1).name,...
-            'filepath',subject.dir);
-    else
+            'filepath',subject.raw_dir);
+        else
         % Send warning:
         exit([subject.id ' - File not found!']);
+        end
     end
-    
 end
 
 %% Add subject information to data structure:
